ae23c44768ffb4223fa4c237296de1ac6bfad6e9
rodrigofries01 <rfries24@gmail.com>
2025-11-11 14:12:26 -0300

    configuracao docker

--- src/main/java/com/utfpr/ofertasdv/OfertasdvApplication.java
+++ src/main/java/com/utfpr/ofertasdv/OfertasdvApplication.java
@@ -1,4 +1,4 @@
-package com.utfpr.ofertasdv.;
+package com.utfpr.ofertasdv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
